978c3218dbf57613795661e0e6720ac376882a61
SpektrumSGA <zk-@live.com.ar>
2021-10-22 23:36:22 -0300

    Double correction

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -12,7 +12,6 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-      allowNull: false,
     },
     flagimg: {
       type: DataTypes.STRING,
